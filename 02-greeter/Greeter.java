0951bbb471b5e43ccc6b6fccd842c704f9c51cea
Rabia Akhtar <rabia1082@gmail.com>
2014-09-15 17:52:49 -0400

    please

---
+++ 02-greeter/Greeter.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Greeter {
+    // We put the attributes here
+    // We put the abilities here
+    // In Java, these are called methods.
+    // methods are functions but they belong to specific classes
+    
+ 
+    public void greet() {
+	System.out.println("Hello world!");
+    }
+    public void ungreet(){
+	System.out.println("I'm Out!");
+
+    }
+
+}
+
