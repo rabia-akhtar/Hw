3abc8b997c89916e999d968f3d00da34729cb05f
Rabia Akhtar <rabia1082@gmail.com>
2014-09-15 17:50:31 -0400

    please

---
+++ 02-greeter/Driver.java
@@ -0,0 +1,43 @@
+import java.io. *;
+import java.util.*;
+
+public class Driver {
+    public static void main (String []args ){
+	// declares a local variable g to be of type Greeter 
+
+	Greeter g;
+	System.out.println(g);
+	g = new Greeter();
+	/* new does the following 
+	   1. Allocates enough memory to store Greete r
+	   2. Do whatever's needed to setup / initialize 
+	   the memory to be Greeter.
+	   3. return the address of the memory that was allocated.
+
+	   The assignment stores the address in g.  
+
+	*/
+	// This prints out the memory location of Greeter 
+	// System.out.println(g);
+	g.greet();
+	g.greet();
+	g.ungreet();
+    }
+}
+
+/* 
+In Java all variables must be declared before use 
+declarations specify the type of variables
+
+Local Variable - declared in a method
+It is created when a function is called and destroyed
+when a function exits and is only visible (usable) inside 
+the method. 
+
+private - can only be accessed from within the class its declared in 
+public - can be accessed from pretty much anywhere
+
+we can gain access to private stuff by calling public methods that 
+can access the private stuff
+*/
+
