766dbe5b9cfb708f5a0a9f655ca896b2989ad6e3
rabia-akhtar <rabia1082@gmail.com>
2014-09-23 20:39:59 -0400

    hw

---
+++ 05-codingbat/Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+    public static void main(String[] args){
+	StringStuff s = new StringStuff();
+	System.out.println(s.nonStart("hi","bye"));
+        System.out.println(s.makeABBA("hi","bye"));
+        System.out.println(s.diff21(5));
+
+    }
+}
