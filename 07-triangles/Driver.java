b4a3b5925225f4828d22da88cc4241be5d85e091
rabia-akhtar <rabia1082@gmail.com>
2014-10-03 03:19:57 -0400

    7b updated

--- 07-triangles/Driver.java
+++ 07-triangles/Driver.java
@@ -6,5 +6,8 @@ public class Driver {
 	System.out.println(s.tri2(4));
 	System.out.println(s.tri1(5));
 	System.out.println(s.tri2(7));
+	System.out.println(s.tri3(5));
+	System.out.println(s.diamond(5));
+	System.out.println(s.tri4(5));
     }
 }
--- 07-triangles/Shapes.java
+++ 07-triangles/Shapes.java
@@ -56,5 +56,82 @@ public class Shapes {
 	}
 	return result;
     }
+    public String tri3(int h) {
+	/* this took around 25 minutes to write because typing with 
+	   henna on is not a good idea */
+	String result="";
+	int i,one,two,three;
+	int n= h;
+	int c=1;
+	for (i=0;i<n;i++,h--){
+	    for (one=h-1;one>0;one--){
+		result +=" ";
+	    }
+	    two=1;
+	    while (two<=(2*c)-1 & c<=((2*n)-1)){
+	    result+="*";
+	    two+=1;
+	}
+	    c+=1;
+	    result+="\n";
+	}
+	return result;
+    }
+    public String diamond(int h){
+	// 30 minutes. i think my code should be cleaner than this...
+	String result = "";
+	int i,one,two,three,f,a,b,d,e;
+	int n= h;
+	int count=h; 
+	int c=h;  
+	int cc=h;  
+	int z=1;
+	int g=h/2;
+	e=h-2;
+	two=0;
+	one=count-1;
+	for (i=0;i<(n-2);i++,count--){
+	    for (one=count-1;one>1;one--){
+		result +=" ";
+	    }
+	    two=1;
+	    while (two<=z && z<=h){
+		result+="*";
+		two+=1;
+	    }
+	    z+=2;	  
+	    result+="\n";
+	}
+	for (a=0;a<g;a++,c++){
+	    for (b=0;b<(c/2);b++){
+		result +=" ";
+	    }
+	    d=1;
+	    while (d<=e && e>=0){
+		result+="*";
+		d++; 
+	    }
+	    e-=2;	  
+	    result+="\n";
+	}
+	return result;
+    }
 
-}
+ public String tri4(int height){
+     // 2 minutes, flipped tri4
+	int h;
+	int i;
+        String s="";
+	for (h=height;h>=0;h--){
+	    for (i=0;i<height-h;i++){
+		s = s + " ";
+	    }
+	    for (;i<height;i++){
+		s=s+"*";
+	    }
+	    s=s+"\n";
+	}
+	return s;
+    }
+
+    }
