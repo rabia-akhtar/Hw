4cf096cd46e7434d339e6728d38aef7d4ed142c8
Rabia Akhtar <rabia1082@gmail.com>
2014-11-13 09:24:39 -0500

    hw

--- 16-arraylists/ArrayLists.java
+++ 16-arraylists/ArrayLists.java
@@ -19,8 +19,5 @@ public static void main (String [] args){
 
 
 
-
-
-
 }
 }
--- 17-AL/Driver.java
+++ 17-AL/Driver.java
@@ -1,11 +1,22 @@
+import java.util.*;
 public class Driver {
     public static void main(String[] args) {
 	Random rand = new Random();
 	ArrayList<Integer> al  = new ArrayList<Integer>();
-	for (int i=0;i<5;i++) {
+	for (int i=1;i<=5;i++) {
 	    al.add(i);
 	}
 	System.out.println(al);
+	
+	for (int h=0;h<al.size();h++) {
+	    int x = al.get(rand.nextInt(5));
+	    int indx = al.indexOf(x);
+	    int valh = al.get(h);
+	    al.set(h,x);
+	    al.set(indx,valh);
+	}
+	System.out.println(al);
+	    
     }
 }
 	    
