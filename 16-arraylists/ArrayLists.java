8e0d25a6016db3a54b9e860e224a4d60ef2badce
Rabia Akhtar <rabia1082@gmail.com>
2014-11-13 08:57:45 -0500

    update

---
+++ 16-arraylists/ArrayLists.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class ArrayLists {
+public static void main (String [] args){
+
+    ArrayList<Integer> ai = new ArrayList<Integer>();
+
+    Random r = new Random();
+    for (int i = 0; i < 20; i++) {
+	ai.add(r.nextInt(5));
+    }
+
+    System.out.println(ai);
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
