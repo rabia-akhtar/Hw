efc37b9a21c9826799717aa5eea09d49e63f2db1
Rabia Akhtar <rabia1082@gmail.com>
2014-11-13 09:14:11 -0500

    hw

---
+++ 17-AL/Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+    public static void main(String[] args) {
+	Random rand = new Random();
+	ArrayList<Integer> al  = new ArrayList<Integer>();
+	for (int i=0;i<5;i++) {
+	    al.add(i);
+	}
+	System.out.println(al);
+    }
+}
+	    
+					   
