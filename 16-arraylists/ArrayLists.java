ac770fcd3807f80dc4df78fd6af23be32c2775ec
rabia-akhtar <rabia1082@gmail.com>
2014-11-13 21:33:45 -0500

    update

--- 16-arraylists/ArrayLists.java
+++ 16-arraylists/ArrayLists.java
@@ -1,23 +1,26 @@
 import java.util.*;
 public class ArrayLists {
-public static void main (String [] args){
+    public static void main (String [] args){
 
-    ArrayList<Integer> ai = new ArrayList<Integer>();
+	ArrayList<Integer> ai = new ArrayList<Integer>();
 
-    Random r = new Random();
-    for (int i = 0; i < 20; i++) {
-	ai.add(r.nextInt(5));
+	Random r = new Random();
+	for (int i = 0; i < 20; i++) {
+	    ai.add(r.nextInt(5));
+	}
+
+	System.out.println(ai);
+	for (int i=0;i<ai.size()-1;i++){
+	    if (ai.get(i) == ai.get(i+1)) ai.remove(i); 
+	}
+	System.out.println(ai);
+
+
+
+
+
+
+
+
     }
-
-    System.out.println(ai);
-
-
-
-
-
-
-
-
-
-}
 }
