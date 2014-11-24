ae74cd5ece2691d2316dbf5a30bf521b1dc929c5
Rabia Akhtar <rabia1082@gmail.com>
2014-11-24 09:23:42 -0500

    hw

--- 18-WordSearch/ReadFile.java
+++ 18-WordSearch/ReadFile.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class ReadFile {
     public static void main(String[] args) {
+	WordSearch w =new WordSearch();
 	Scanner sc = null;
 	try {
 	    sc =new Scanner(new File("words.txt"));
@@ -12,8 +13,8 @@ public class ReadFile {
 	}
 	while (sc.hasNext()) {
 	    String s = sc.next();
-	    System.out.println("Next line: "+s);
-						
+	    w.addWord(s);						
 	}
+	System.out.println(w);
     }
 }
--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -101,13 +101,9 @@ public class WordSearch {
 	}
 	catch (Exception e){
 	    return false;
-		}
+	}
 
     }
-    public static void main(String[] args) {
-	WordSearch w = new WordSearch();
-	w.addWord("hello"); 
-	w.addWord("look"); 
-	System.out.println(w);
-    }
+  
+
 }
