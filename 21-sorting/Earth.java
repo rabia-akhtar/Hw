2973fb22243eeaaf54c5bf40bb425cb72f05ff28
rabia-akhtar <rabia1082@gmail.com>
2014-12-11 00:09:52 -0500

    21b

--- 21-sorting/Earth.java
+++ 21-sorting/Earth.java
@@ -1,9 +1,13 @@
-public class Earth extends Basechar{
+public abstract class Earth extends Basechar implements Comparable{
     public Earth(){
+	super();
 	setElement("earth");
 	setA1name("boulder throw");
 	setA2name("rock block");
 	setA3name("solidify");
 	setA4name("earthquake");
+    }
+    public int CompareTo(Earth other){
+	return (this.getName()).compareTo(other.getName());
     }
 }
